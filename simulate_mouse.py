80049c2cd6576c8721b990b14c341a78741d160f
yagueteiro <yagofreirefdez@gmail.com>
2020-10-13 11:45:51 +0200

    Added GUI with tkinter

--- simulate_mouse.py
+++ simulate_mouse.py
@@ -1,8 +1,11 @@
+import threading
 import pyautogui
 import random
 import time
 import argparse
 import sys
+import tkinter as tk
+from argparse import Namespace
 
 # defines user input via. cli
 parser = argparse.ArgumentParser()
@@ -24,6 +27,18 @@ sleep_range = (args.sleep_min, args.sleep_max)
 # get screen dimensions
 display_width, display_height = pyautogui.size()
 
+window = tk.Tk()
+
+window.title("Simulation")
+
+window.geometry("500x200")
+
+simulation_state = "STOPPED"
+
+radiobuttonvar = tk.StringVar(value="comb")
+
+executing = tk.IntVar(value=0)
+
 
 def exit_prompt() -> str:
     """Exit prompt to check if user wants to exit"""
@@ -35,11 +50,14 @@ def exit_prompt() -> str:
 def simulate_mouse(width=display_width, height=display_height, duration=duration_range, sleep=sleep_range,
                    one_move=False):
     """simulate mouse movement randomly across the screen"""
+    global simulation_state
     while True:
+        if simulation_state == "STOPPED":
+            break
         # adjust screen - 10% padding
-        pyautogui.moveTo(random.randint(width*0.1, width*0.9),
-                         random.randint(height*0.1, height*0.9),
-                         random.randint(duration[0], duration[1])/10)
+        pyautogui.moveTo(random.randint(width * 0.1, width * 0.9),
+                         random.randint(height * 0.1, height * 0.9),
+                         random.randint(duration[0], duration[1]) / 10)
         time.sleep(random.randint(sleep[0], sleep[1]))
         if one_move:
             break
@@ -47,9 +65,11 @@ def simulate_mouse(width=display_width, height=display_height, duration=duration
 
 def simulate_keys_volume(sleep=sleep_range, one_move=False):
     """simulate key presses"""
+    global simulation_state
 
     while True:
-
+        if simulation_state == "STOPPED":
+            break
         # increase volume and then decrease it to get original value
         pyautogui.press('volumeup')
         time.sleep(1)
@@ -63,7 +83,10 @@ def simulate_keys_volume(sleep=sleep_range, one_move=False):
 
 def simulate_combined(width=display_width, height=display_height, duration=duration_range, sleep=sleep_range):
     """Simulates mouse and volume in combination - via. random choice"""
+    global simulation_state
     while True:
+        if simulation_state == "STOPPED":
+            break
         rand_choice = random.choice(["vol", "mouse"])
         if rand_choice == "vol":
             simulate_keys_volume(sleep, one_move=True)
@@ -92,5 +115,49 @@ def simulate(argu=args):
             simulate()
 
 
+def start_simulation():
+    global simulation_state, radiobuttonvar
+    if not simulation_state == "RUNNING":
+        simulation_state = "RUNNING"
+        threading.Thread(target=simulate, args=(Namespace(mode=str(radiobuttonvar.get())),)).start()
+
+
+def stop_simulation():
+    global simulation_state
+    simulation_state = "STOPPED"
+    window.destroy()
+
+
+def create_gui():
+    global radiobuttonvar
+    title_frame = tk.Frame(window)
+    title_frame.pack(fill="x", expand=True)
+
+    button_frame = tk.Frame(window)
+    button_frame.pack(fill='both', expand=True)
+
+    bottom_frame = tk.Frame(window)
+    bottom_frame.pack()
+
+    type_frame = tk.Frame(bottom_frame)
+    type_frame.pack(fill="both", expand=True, side=tk.LEFT)
+
+    title = tk.Label(title_frame, text="Select a simulation type and press run")
+
+    run = tk.Radiobutton(button_frame, text="Run", command=start_simulation, indicatoron=0, variable=executing, value=1)
+    stop = tk.Radiobutton(button_frame, text="Stop", command=stop_simulation, indicatoron=0, variable=executing,
+                          value=0)
+
+    tk.Radiobutton(type_frame, text="Combined", variable=radiobuttonvar, value="comb").pack(anchor='w', fill="both")
+    tk.Radiobutton(type_frame, text="Mouse", variable=radiobuttonvar, value="mouse").pack(anchor='w')
+    tk.Radiobutton(type_frame, text="Volume", variable=radiobuttonvar, value="vol").pack(anchor='w')
+
+    title.pack()
+    run.pack(side="left", fill="x", expand=True)
+    stop.pack(side="left", fill="x", expand=True)
+    window.wm_protocol("WM_DELETE_WINDOW", stop_simulation)
+
+
 if __name__ == "__main__":
-    simulate()
+    create_gui()
+    window.mainloop()
